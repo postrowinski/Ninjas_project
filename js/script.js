e8bdd8e3405557d97eca14537ff17b7a001590b0
Przemek <p.ostrowinski@gmail.com>
2017-10-21 22:40:40 +0200

    add all sections and RWD

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
-const handleAppsLinksClick = document.querySelectorAll('.slide-button-buy a');
+document.addEventListener('DOMContentLoaded', function() {
+    const handleAppsLinksClick = document.querySelectorAll('.slide-button-buy a');
+    const footer = document.querySelector('footer');
+    const footerImage = document.querySelector('.rugged-paper-upside-down');
+    var footerHeight = footer.clientHeight;
 
-//Prevent to moving website after clicking to top of the window
-handleAppsLinksClick.forEach((element) => {
-    element.addEventListener('click', event => event.preventDefault());
+    footerImage.style.bottom = footerHeight + 'px';
+
+
+    window.addEventListener('resize',function () {
+        var footerHeight = footer.clientHeight;
+        footerImage.style.bottom = footerHeight + 'px';
+    });
+
+    //Prevent to moving website after clicking to top of the window
+    handleAppsLinksClick.forEach(function (element) {
+        element.addEventListener('click', function (event) {
+            event.preventDefault()
+        });
+    });
 });
+//     footerImage.style.bottom = footerHeight + 'px';
+//
+//     function appSlider() {
+//         var $carouselRow = $('#app-carousel .row');
+//         var $sliceWidth = $carouselRow.children().first().clientWidth;
+//         var slideTime = 3000;
+//
+//
+//         window.addEventListener('resize', function () {
+//             var $sliceWidth = $carouselRow.children().first().clientWidth;
+//         });
+//
+//
+//         function changeMargin() {
+//
+//         }
+//
+//         function nextSlide() {
+//             var $firstSlide = $carouselRow.find('.slide-container').first();
+//             var $lastSlide = $carouselRow.find('.slide-container').last();
+//
+//             $lastSlide.after($firstSlide);
+//             changeMargin();
+//         }
+//     }
+// });
